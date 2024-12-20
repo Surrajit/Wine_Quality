8328512eab4660de30801a7f1d4f8b1ab878380b
Surrajit <152585200+Surrajit@users.noreply.github.com>
2024-12-20 21:24:16 +0530

    Data Valiadation Completed

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from src.wine_quality import logger
 from src.wine_quality.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
-
+from src.wine_quality.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.initiate_data_validation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
